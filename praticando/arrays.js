df4a761ac6d66c26fddfaedd99c8b8cb6397a40c
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2022-12-16 14:23:40 -0300

    array-js

---
+++ praticando/arrays.js
@@ -0,0 +1,16 @@
+// array
+
+const animals = [
+  "Lion",
+  "Monkey",
+  {
+    name: "cat",
+    age: 3,
+  },
+];
+
+// acessing values inside the array - acessando valores dentro do array
+console.log(animals[2]);
+
+// total arrays elements - total de elementos array
+console.log(animals.length);
