06a34f568dfcc9ccf145482788a219685051b43e
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2022-12-17 19:23:12 -0300

    functions - js

---
+++ functions/functions.js
@@ -0,0 +1,18 @@
+// criar um aplicativo de frases motivacionais
+
+// function statement - declaração de função
+function createPhrases() {
+  console.log("Estudar é muito bom");
+  console.log("Paciência e persistência");
+  console.log("Revisão é a mãe do aprendizado");
+}
+
+/* execute the function (can be other names: run, call, invoke the function)
+   executar a função (pode ser outros nomes: rodar, chamar, invocar a função) */
+
+createPhrases();
+
+// repeating the code - repetindo o código
+createPhrases();
+
+console.log("fim do programa");
