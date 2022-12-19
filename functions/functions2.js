4fcfd7bbdb50d58dec9d5c0833b0610e97124c43
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2022-12-19 17:42:07 -0300

    agrupamentos e parâmetros - js

---
+++ functions/functions2.js
@@ -0,0 +1,9 @@
+// function expression -  expressão de função
+// function anonymous - função anônima
+
+// parameters - parâmetros
+const sum = function (number1, number2) {
+  console.log(number1 + number2);
+};
+
+sum(2, 3); // arguments - argumentos
