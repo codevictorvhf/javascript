6e0b4dca284a4e3a59ea9fbe262782f328a0aee4
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-20 18:40:21 -0300

    assignment operators-js

---
+++ expressions-and-operators/assignment-operators.js
@@ -0,0 +1,30 @@
+// Operadores de atribuição assignment
+let x;
+
+// assignment
+x = 1;
+console.log(x);
+
+// addition assignment
+x += 2;
+console.log(x);
+
+// subtraction assingnment
+x -= 1;
+console.log(x);
+
+// multiplication assignment
+x *= 2;
+console.log(x);
+
+// division assignment
+x /= 2;
+console.log(x);
+
+// remainder division
+x %= 2;
+console.log(x);
+
+// exponetiation
+x **= 2;
+console.log(x);
