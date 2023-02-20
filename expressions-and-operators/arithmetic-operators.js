3655b6b05d8cafac297062cf019130294034ba77
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-20 17:30:42 -0300

    aithmetic-operators.js

---
+++ expressions-and-operators/arithmetic-operators.js
@@ -0,0 +1,31 @@
+// Multiplicação
+console.log(8 * 7);
+
+// Divisão
+console.log(12 / 2);
+
+// Soma
+console.log(34 + 67);
+
+// Subtração
+console.log(34 - 23);
+
+// Resto da divisão
+let remainder;
+remainder = 11 % 9;
+console.log(remainder);
+
+// Incremento
+let increment = 0;
+increment++;
+increment++;
+console.log(++increment);
+
+// Decremento
+let decrement = 0;
+decrement--;
+decrement--;
+console.log(--decrement);
+
+// Exponencial
+console.log(2 ** 3);
