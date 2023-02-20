d713904529c2ad26c0b10b742f2dea9d2ee9c44b
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-20 19:30:58 -0300

    ternary-conditional-js

---
+++ expressions-and-operators/ternary.conditional-operator.js
@@ -0,0 +1,10 @@
+// café da manha top
+let pao = true;
+let queijo = true;
+
+const niceBreakFest = pao && queijo ? "Café top" : "Café ruim";
+console.log(niceBreakFest);
+
+let age = 16;
+const canDrive = age >= 18 ? "Can drive" : "Can't drive";
+console.log(canDrive);
