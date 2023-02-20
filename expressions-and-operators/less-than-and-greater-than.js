4863ede9a3a5829c73432fb4c5fb7cc413142e1a
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-20 18:13:31 -0300

    less-than-and-greater-than-js

---
+++ expressions-and-operators/less-than-and-greater-than.js
@@ -0,0 +1,18 @@
+let one = 1;
+let two = 2;
+
+// > maior que
+console.log(one > two);
+
+// < maior ou igual a
+console.log(one >= 1);
+console.log(two >= 1);
+
+// menor que
+console.log(one < two);
+
+// menor ou igual a
+
+console.log(one <= two);
+console.log(one <= 1);
+console.log(one <= 0);
