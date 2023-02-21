e94ebea5f72002cc2ed9ad3c807d4800ccde8db3
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-21 03:07:51 -0300

    operator-precedence-js

---
+++ expressions-and-operators/operator-precedence.js
@@ -0,0 +1,19 @@
+/* Precedência de Operadores
+
+1- grouping ()
+2- negação e incremento ! ++ --
+3- multiplicação e divisão * /
+4- adição e subtração + - 
+5- relacional < <= > >=
+6- igualdade == != === !==
+7- AND &&
+8- OR ||    
+9- condicional ? :
+10- assignment (atribuição) = += -= *=
+*/
+
+console.log(2 + 5 * 10);
+
+console.log(3 > 2 == 1);
+
+console.log(3 > 2 && 2 > 1);
