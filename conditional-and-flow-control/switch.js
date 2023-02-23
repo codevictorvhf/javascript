7f62f2e9d2ea07de35f38148ba0795aef44c68db
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-02-22 22:25:37 -0300

    switch-and-calculator-js

---
+++ conditional-and-flow-control/switch.js
@@ -0,0 +1,17 @@
+let expression = "a";
+
+switch (expression) {
+  case "a":
+    // codigo
+    console.log("a");
+    break;
+
+  case "b":
+    // codigo
+    console.log("b");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
