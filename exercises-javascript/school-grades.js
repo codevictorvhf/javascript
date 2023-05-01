33f8639015770025b3248870e416eb2725a1826f
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-05-01 17:58:51 -0300

    school-grades.js

--- exercises-javascript/school-grades.js
+++ exercises-javascript/school-grades.js
@@ -1,25 +1,32 @@
-let score = 77;
+function getScore(score) {
+  let a = score >= 90;
+  let b = score >= 80 && score <= 89;
+  let c = score >= 70 && score <= 79;
+  let d = score >= 60 && score <= 69;
+  let e = score < 60 && score >= 0;
 
-let a = score >= 90;
-let b = score >= 80 && score <= 89;
-let c = score >= 70 && score <= 79;
-let d = score >= 60 && score <= 69;
-let e = score < 60 && score >= 0;
+  let scoreFinal;
 
-let scoreFinal;
+  if (a) {
+    scoreFinal = "A";
+  } else if (b) {
+    scoreFinal = "B";
+  } else if (c) {
+    scoreFinal = "C";
+  } else if (d) {
+    scoreFinal = "D";
+  } else if (e) {
+    scoreFinal = "E";
+  } else {
+    scoreFinal = "Invalid Note";
+  }
 
-if (a) {
-  scoreFinal = "A";
-} else if (b) {
-  scoreFinal = "B";
-} else if (c) {
-  scoreFinal = "C";
-} else if (d) {
-  scoreFinal = "D";
-} else if (e) {
-  scoreFinal = "E";
-} else {
-  scoreFinal = "Invalid Note";
+  return scoreFinal;
 }
 
-console.log(scoreFinal);
+console.log(getScore(2));
+console.log(getScore(77));
+console.log(getScore(91));
+console.log(getScore(64));
+console.log(getScore(88));
+console.log(getScore(-2));
