b459e9d76f9df666d76b1f2bf9d3d87c2513a02c
Victor Henrique <101061730+codevictorvhf@users.noreply.github.com>
2023-05-05 02:35:14 -0300

    att-family-cash.js

---
+++ exercises-javascript/family-cash-flow.js
@@ -0,0 +1,31 @@
+let family = {
+  revenues: [500, 100, 430.5, 3500],
+  expenses: [450, 300, 110.15, 2200],
+};
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+
+function calculateBalance() {
+  const calculateRevenues = sum(family.revenues);
+  const calculateExpenses = sum(family.expenses);
+
+  const total = calculateRevenues - calculateExpenses;
+
+  const positive = total >= 0;
+
+  let balanceText = "Negative";
+
+  if (positive) {
+    balanceText = "Positive";
+  }
+}
+
+console.log(`Seu saldo é ${balanceText}: ${total}`);
+
+calculateBalance();
