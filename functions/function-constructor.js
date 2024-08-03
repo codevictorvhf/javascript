6fa2e8e7797381196bf681a4ab647dd830e3980d
“Victor <victorvf11@outlook.com>
2024-08-02 21:19:43 -0300

    .

--- functions/function-constructor.js
+++ functions/function-constructor.js
@@ -1,14 +1,28 @@
-function Person(name) {
-  this.name = name;
-  this.walk = function () {
+// function Person(name) {
+//   this.name = name;
+//   this.walk = function () {
+//     return this.name + " is walking";
+//   };
+// }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.walk();
+  }
+
+  walk() {
     return this.name + " is walking";
-  };
+  }
 }
 
 const victor = new Person("Victor");
 const carol = new Person("Carol");
 console.log(victor.walk());
 console.log(carol.walk());
+
+victor.name = "michael";
+console.log(victor.walk());
 
 let string = new String("Victor");
 console.log(string);
